a1bb1565de6a0930bef0d76c8075df0fe45aa8ba
Michael Puerto <miklpuerto69@gmail.com>
2025-08-06 18:13:26 -0400

    Add Precommit hook

---
+++ src/activitytracker/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+Utility functions and helpers for the ActivityTracker application.
+
+This module contains shared utility functions, constants, and helper classes
+that are used across different parts of the ActivityTracker application.
+"""
+
+__version__ = "0.1.0"
